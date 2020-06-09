1e419e1f8a27f87fda4e3c676bbae2e671ca185b
syedshajihaider <syedshajihaider@gmail.com>
2020-06-09 18:13:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Covid19 Tracking App By Syed Shaji Haider
         </p>
         <a
           className="App-link"
